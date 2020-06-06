c338b19777f126e58c2c2aee042eb946455cf2c6
Emiliano Monti <emilianomonti95@gmail.com>
2020-06-06 11:07:07 +0200

    creazione json per accodamento a redis

--- Gateway/Receiver/app.js
+++ Gateway/Receiver/app.js
@@ -7,25 +7,40 @@ const port = new SerialPort('/dev/ttyS0');
 const parser = port.pipe(new ByteLength({ length: 2 }))
 parser.on('data', parseMsg)
 
+const slotGateway = 'slot00';
+
 function parseMsg(data) {
-	console.log(data);
+	//console.log(data);
 
 	//per ogni elemento del buffer ricevuto via seriale, estraiamo il valore in binario
 	let msgSize = data.length;
 
 	//creiamo due variabili dove inserire il balore binario dei due byte
 	let byte0 = parseInt(data[0], 10).toString(2).padStart(8, '0');
-	let byte1 = parseInt(data[1], 10).toString(2).padStart(8, '0');
-	let byte2 = parseInt(data[2], 10).toString(2).padStart(8, '0');
+	//let byte1 = parseInt(data[1], 10).toString(2).padStart(8, '0');
+	//let byte2 = parseInt(data[2], 10).toString(2).padStart(8, '0');
 
-	console.log("byte 1 ", byte0); //incrocio
-	console.log("byte 2 ", byte1); //valore
-	console.log("byte 3 ", byte2); //gateway + id sensore
+	console.log("byte 1 ", byte0); //gateway + id sensore
+	console.log("byte 2 ", byte1); //id incrocio
+	console.log("byte 3 ", byte2); //valore
 
 	//ipotizziamo il protocollo indicato nell'esercitazione del  prof. Bortolani
-	let gateway = byte2.substring(0, 4); //primi 4 bit del byte : COMANDO
-	let sensore = byte2.substring(4);   //ultimi 4 bit del byte : ID Sensore / Attuatore
+	let gateway = byte0.substring(0, 4); //primi 4 bit del byte : COMANDO
+	let sensore = byte0.substring(4);   //ultimi 4 bit del byte : ID Sensore / Attuatore
 
-	console.log(gateway);
+	var id = data[1];
+	var valore = data[2];
+	let json = {};
+
+	if (gateway == 'slot00') {
+
+		json = {
+			"id_incrocio": id,
+			"Semaforo": sensore,
+			"Valore": valore
+		};
+
+	}
+	console.log(json);
 
 }
