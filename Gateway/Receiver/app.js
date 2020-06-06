63224f58a27f18e941a37747eb5f2d01e0d12566
Emiliano Monti <emilianomonti95@gmail.com>
2020-06-06 10:43:37 +0200

    prima lettura byte

--- Gateway/Receiver/app.js
+++ Gateway/Receiver/app.js
@@ -12,41 +12,20 @@ function parseMsg(data) {
 
 	//per ogni elemento del buffer ricevuto via seriale, estraiamo il valore in binario
 	let msgSize = data.length;
-	for (let i = 0; i < msgSize; i++) {
-		let valore = parseInt(data[i], 10).toString(2);
-		//aggiungiamo gli 0 omessi perchè non significanti
-		console.log(valore.padStart(8, '0'));
-	}
 
 	//creiamo due variabili dove inserire il balore binario dei due byte
 	let byte0 = parseInt(data[0], 10).toString(2).padStart(8, '0');
 	let byte1 = parseInt(data[1], 10).toString(2).padStart(8, '0');
+	let byte2 = parseInt(data[2], 10).toString(2).padStart(8, '0');
+
+	console.log("byte 1 ", byte0); //incrocio
+	console.log("byte 2 ", byte1); //valore
+	console.log("byte 3 ", byte2); //gateway + id sensore
 
 	//ipotizziamo il protocollo indicato nell'esercitazione del  prof. Bortolani
-	let comando = byte0.substring(0, 4); //primi 4 bit del byte : COMANDO
-	let sensore = byte0.substring(4);   //ultimi 4 bit del byte : ID Sensore / Attuatore
+	let gateway = byte2.substring(0, 4); //primi 4 bit del byte : COMANDO
+	let sensore = byte2.substring(4);   //ultimi 4 bit del byte : ID Sensore / Attuatore
 
-	//tabella dei comandi
-	switch (comando) {
-		case "0000":
-			console.log("Scrivi dato");
-			break;
-		case "0001":
-			console.log("Leggi dato");
-			break;
-		case "0101":
-			console.log("Conferma dato");
-			break;
-		case "0110":
-			console.log("Vis. Menu");
-			break;
-		case "1100":
-			console.log("Risposta");
-			break;
-
-	}
-
-	//etc etc
-
+	console.log(gateway);
 
 }
