1adae88b680369107a91e001db420d80f36534f7
Emiliano Monti <emilianomonti95@gmail.com>
2020-07-01 17:58:08 +0200

    add trigger from SQL DAM to queue

--- PWInfoStradeFunctions/SQLQueryToDAM.cs
+++ PWInfoStradeFunctions/SQLQueryToDAM.cs
@@ -11,39 +11,51 @@ using Microsoft.Extensions.Configuration;
 
 namespace PWInfoStradeFunctions
 {
-    public static class SQLQueryToDAM
-    {
-        [FunctionName("SQLQueryToDAM")]
-        public static async Task Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)  //0 0 * * * *
-        {
-            log.LogInformation($"Function Trigger viene eseguita ogni ora partendo da: {DateTime.Now}");
+    //public static class SQLQueryToDAM
+    //{
+    //    [FunctionName("SQLQueryToDAM")]
+    //    public static async Task Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)  //0 0 * * * *
+    //    {
+    //        log.LogInformation($"Function Trigger viene eseguita ogni ora partendo da: {DateTime.Now}");
 
-            var config = new ConfigurationBuilder().SetBasePath(context.FunctionAppDirectory).
-                    AddJsonFile("local.settings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables().Build();
+    //        var config = new ConfigurationBuilder().SetBasePath(context.FunctionAppDirectory).
+    //                AddJsonFile("local.settings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables().Build();
 
-            //var str = ConfigurationManager.ConnectionStrings["sqldb_connection"].ConnectionString;
+    //        //var str = ConfigurationManager.ConnectionStrings["sqldb_connection"].ConnectionString;
 
-            var str = config["sqldb_connection"];
+    //        var str = config["sqldb_connection"];
 
-            using (SqlConnection conn = new SqlConnection(str))
-            {
-                conn.Open();
-                var query =
-                    "INSERT INTO [SalesLT].[Product] " +
-                   "( [Name] " +
-                   ", [ProductNumber] " +
-                   ", [Color] " +
-                   ", [ProductCategoryID] " +
-                   ", [StandardCost] " +
-                   ", [ListPrice] " +
-                   ", [SellStartDate] ) ";
+    //        using (SqlConnection conn = new SqlConnection(str))
+    //        {
+    //            conn.Open();
+    //            var query = "";
+    //            //{
+    //                //INSERT INTO OPENDATASOURCE(
 
-                using (SqlCommand cmd = new SqlCommand(query, conn))
-                {
-                    var result = await cmd.ExecuteNonQueryAsync();
-                    log.LogInformation($"{result}");
-                }
-            }
-        }
-    }
+    //                //    'SQLNCLI', 'Server=pwsmartcross.database.windows.net;Database=smartcross;UID=its2020;PWD==Projectwork2020;').smartcross.dbo.Traffico
+    //                //SELECT
+
+    //                //    Id_incrocio,
+	   //                // Id_semaforo,
+	   //                // Id_strada,
+	   //                // Fascia_oraria,
+	   //                // Data,
+	   //                // Tipologia_veicolo,
+	   //                // SUM(Conteggio) AS Conteggio
+    //                //FROM dbo.TrafficTable
+    //                //WHERE Fascia_oraria = 12
+    //                //GROUP BY Fascia_oraria, Id_incrocio, 
+	   //                // Id_semaforo, Id_strada, Data, 
+	   //                // Tipologia_veicolo, Conteggio
+    //                //ORDER BY Fascia_oraria asc
+    //            //}
+                    
+    //            using (SqlCommand cmd = new SqlCommand(query, conn))
+    //            {
+    //                var result = await cmd.ExecuteNonQueryAsync();
+    //                log.LogInformation($"{result}");
+    //            }
+    //        }
+    //    }
+    //}
 }
---
+++ PWInfoStradeFunctions/SQlToDeviceMessage.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Data;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace PWInfoStradeFunctions
+{
+    public static class SQlToDeviceMessage
+    {
+        [FunctionName("SQlToDeviceMessage")]
+        public static void Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        {
+
+            log.LogInformation($"Function Trigger viene eseguita ogni ora partendo da: {DateTime.Now}");
+
+            var config = new ConfigurationBuilder().SetBasePath(context.FunctionAppDirectory).
+                    AddJsonFile("local.settings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables().Build();
+
+            //var str = ConfigurationManager.ConnectionStrings["sqldb_connection"].ConnectionString;
+
+            var connString = config["sqldb_connection_DAM"];
+            string sprocname = "RetrievePerfCounterData";
+            string jsonOutputParam = "@jsonOutput";
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                //var query =
+
+                //    "SELECT temporizzazione.[Id_temporizzazione] " +
+                //    ",temporizzazione.[Id_incrocio] " +
+                //    ",temporizzazione.[Id_semaforo] " +
+                //    ",temporizzazione.[Fascia_oraria] " +
+                //    ",temporizzazione.[Valore_tempo] " +
+                //    "FROM [dbo].[Temporizzazione] temporizzazione " +
+                //    "WHERE Id_incrocio = 4 AND Fascia_oraria = " + ora + " " +
+                //    "FOR JSON AUTO;";
+
+                using (SqlCommand cmd = new SqlCommand(sprocname, conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add(jsonOutputParam, SqlDbType.NVarChar, -1).Direction = ParameterDirection.Output;
+
+                    cmd.ExecuteNonQuery();
+
+                    string json = cmd.Parameters[jsonOutputParam].Value.ToString();
+
+                    
+
+                    log.LogInformation($"Il JSON della procedura è: { json }");
+                }
+            }
+        }
+    }
+}
