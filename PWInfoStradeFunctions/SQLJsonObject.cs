8b8a1ef53a4d3519d8eb66668c119bf9cabbc0e9
Emiliano Monti <emilianomonti95@gmail.com>
2020-07-06 16:00:55 +0200

    Update device twin

---
+++ PWInfoStradeFunctions/SQLJsonObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PWInfoStradeFunctions
+{
+    public class SQLJsonObject
+    {
+        public int Id_temporizzazione { get; set; }
+        public int Id_incrocio { get; set; }
+        public bool Id_semaforo { get; set; }
+        public int Fascia_oraria { get; set; }
+        public int Valore_tempo { get; set; }
+    }
+}
--- PWInfoStradeFunctions/SQlToDeviceMessage.cs
+++ PWInfoStradeFunctions/SQlToDeviceMessage.cs
@@ -1,17 +1,20 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
+using Microsoft.Azure.Devices;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace PWInfoStradeFunctions
 {
     public static class SQlToDeviceMessage
     {
         [FunctionName("SQlToDeviceMessage")]
-        public static void Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        public static async System.Threading.Tasks.Task RunAsync([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
 
             log.LogInformation($"Function Trigger viene eseguita ogni ora partendo da: {DateTime.Now}");
@@ -41,17 +44,37 @@ namespace PWInfoStradeFunctions
 
                 using (SqlCommand cmd = new SqlCommand(sprocname, conn))
                 {
-                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.CommandType = CommandType.StoredProcedure;
 
                     cmd.Parameters.Add(jsonOutputParam, SqlDbType.NVarChar, -1).Direction = ParameterDirection.Output;
 
                     cmd.ExecuteNonQuery();
 
-                    string json = cmd.Parameters[jsonOutputParam].Value.ToString();
+                    var json = cmd.Parameters[jsonOutputParam].Value.ToString();
 
-                    
+                    log.LogInformation($"Il JSON ritornato della procedura è: { json }");
 
-                    log.LogInformation($"Il JSON della procedura è: { json }");
+                    List<SQLJsonObject> obj = JsonConvert.DeserializeObject<List<SQLJsonObject>>(json);
+
+                    var date = DateTime.Now;
+                    foreach (var i in obj)
+                    {
+                        if (i.Fascia_oraria == date.Hour)
+                        {
+                            using (var manager = RegistryManager.CreateFromConnectionString("HostName=IoTHubTrafficLight.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=2NwrhbPzHZALsUpPNQWvVdbZyH4spDvGfnOAVw0JSBI="))
+                            {
+                                var twin = await manager.GetTwinAsync(i.Id_incrocio.ToString());
+                                twin.Properties.Desired["Temporizzazione_verde"] = i.Valore_tempo;
+                                await manager.UpdateTwinAsync(twin.DeviceId, twin, twin.ETag);
+                            }
+                        }
+                     
+                    }
+
+                 //SQLJsonObject temporizzazione = new SQLJsonObject();
+                    //string jsonFilter = JsonConvert.SerializeObject(temporizzazione, Formatting.Indented);
+                    //log.LogInformation($"il JSON filtrato è: {jsonFilter}");
+
                 }
             }
         }
